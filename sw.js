91ac9914a1501c01cebad051fe742ec440446f3c
Twoje Imię <twoj@email.com>
2025-12-09 21:09:42 +0100

    Updates

--- sw.js
+++ sw.js
@@ -19,8 +19,8 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
   );
 });
 
